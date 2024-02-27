e7212dc15018afbb9d66b11d11bd53a47fb95317
Tiago Pires <tiago.pires3006@gmail.com>
2024-02-27 19:35:53 -0300

    Fix Seguros e maquinas conteudo

--- src/pages/para_voce/SeguroMaquinasEquipamentos.tsx
+++ src/pages/para_voce/SeguroMaquinasEquipamentos.tsx
@@ -23,7 +23,7 @@ function SeguroMaquinasEquipamentos() {
                 <div className='container'>
                     <div className='title'>
                         <h1>Seguro Máquinas e Equipamentos</h1>
-                        <p>Há uma grande variedade de equipamentos utilizados para os mais diversos fins na indústria mecânica, na construção civil e nas mais diversas atividades produtivas,</p>
+                        <p>O seguro tem por objetivo garantir, dentro dos limites de responsabilidade da seguradora, sob as Condições Gerais de Riscos Diversos, Condições Especiais e Particulares destacadas, o pagamento de indenização ao segurado, por prejuízos que este possa sofrer em consequência direta da realização de riscos previstos e cobertos.</p>
                         <BannerButtons />
                     </div>
                 </div>
@@ -66,23 +66,13 @@ function SeguroMaquinasEquipamentos() {
                     <div className='box-text'>
                         <h2>SEGUROS PARA EQUIPAMENTOS  AGRÍCOLAS - Penhor Rural</h2>
                         <p>O seguro de penhor rural garante, nos termos das Condições Gerais e das demais condições da apólice, até os Limites Máximos de Indenização das coberturas especificadas na mesma e respeitado o seu Valor em Risco, o pagamento de indenização ao segurado, ou ao beneficiário indicado, por prejuízos consequentes de perdas e danos de origem súbita, imprevista e acidental, diretamente decorrentes dos riscos especificados como cobertos em alguma das coberturas contratadas. Bens Seguráveis:</p>
-                        <p> Este seguro destina-se a conceder cobertura a máquinas e equipamentos de utilização agrícola, florestal, aquícola e agropecuária, oferecidos em garantia de operações de crédito rural, cujo agente seja instituição financeira, conforme as modalidades a seguir:</p>
                     </div>
                 </div>
                 <div className='box'>
                     <div className='box-text'>
+                        <p>Este seguro destina-se a conceder cobertura a máquinas e equipamentos de utilização agrícola, florestal, aquícola e agropecuária, oferecidos em garantia de operações de crédito rural, cujo agente seja instituição financeira, conforme as modalidades a seguir:</p>
                         <p>a) equipamentos estacionários – são as máquinas e/ou equipamentos industriais e comerciais de “tipo fixo” quando instalados para operação permanente em local determinado, de propriedade ou sob controle do arrendatário ou cessionário, expressamente indicado na apólice; e</p>
-                        <p>a) equipamentos estacionários – são as máquinas e/ou equipamentos industriais e comerciais de “tipo fixo” quando instalados para operação permanente em local determinado, de propriedade ou sob controle do arrendatário ou cessionário, expressamente indicado na apólice; e</p>
-                        <ul>
-                            <li>unidades de tração (tratores);</li>
-                            <li>unidades escavo-empurradoras;</li>
-                            <li>unidades escavo-transportadoras;</li>
-                            <li>unidades escavo-carregadoras;</li>
-                            <li>unidades aplanadoras;</li>
-                            <li>unidades de transportes; e</li>
-                            <li>unidades compactadoras.</li>
-                        </ul>
-                        <p> As mesmas Condições Gerais se aplicam a todos os equipamentos, variando, no entanto, de acordo com as modalidades específicas.</p>
+                        <p>b) equipamentos móveis – são os equipamentos que operam por autopropulsão, deslocando-se por seus próprios meios, em canteiros de obras, entre locais de trabalho ou viagem de entrega.</p>
                     </div>
                     <img src={img4} alt='Equipamentos' />
                 </div>
@@ -93,10 +83,6 @@ function SeguroMaquinasEquipamentos() {
                         <p>O Seguro de Crédito Rural ou Crédito é uma modalidade de seguro que tem por objetivo ressarcir o segurado (credor), nas operações de crédito realizadas dentro do território nacional, das perdas líquidas definitivas causadas por devedor insolvente.</p>
                         <p>Em outras palavras, o objetivo do Seguro de Crédito é garantir ao segurado (credor) as perdas líquidas definitivas resultantes do não recebimento de valores relativos às operações de crédito realizadas pelo credor aos seus diversos clientes (devedores).</p>
                         <p>O Seguro de Crédito sempre pressupõe a participação do credor nas perdas. Sendo assim, a indenização contemplará somente parte do crédito,descontando-se, eventualmente, valores pagos antecipadamente.</p>
-                        <ul>
-                            <li>máquinas, equipamentos e implementos autopropulsores, rebocáveis, móveis ou estacionários;</li>
-                            <li>veículos rurais de carga.</li>
-                        </ul>
                     </div>
                     <img src={img5} alt='Equipamentos' />
                 </div>
