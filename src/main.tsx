e2e7b5d4c038008d623932b02bf568dcc9e34092
Tiago Pires <tiago.pires3006@gmail.com>
2025-02-18 10:34:04 -0300

    Add GTM

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-M5BNH86T'
+}
+ 
+TagManager.initialize(tagManagerArgs)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
